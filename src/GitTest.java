93564d13e8d628976fd1a7627e69dd1f299665de
Hakki <hakkiyaz@yahoo.com>
2019-09-17 20:52:59 -0400

    1. commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,11 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("1");
+        for (int i = 0; i <10 ; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("GIT ");
+            }
+            System.out.println();
+        }
     }
 }
