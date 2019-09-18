8cecbb2cc8e036249d8ac257ae7d268090d2d6b1
yaz <52636425+yazHak@users.noreply.github.com>
2019-09-17 21:37:30 -0400

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,8 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
 
-        for (int i = 0; i <10 ; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i <5 ; i++) {
+            for (int j = 0; j < 5; j++) {
                 System.out.print("GIT ");
             }
             System.out.println();
